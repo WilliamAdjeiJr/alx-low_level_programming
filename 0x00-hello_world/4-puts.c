2e404aeb0eaa5fad35e4e923bca895173e53be3b
William Adjei Jr <williamadjeijunior@gmail.com>
2022-09-08 15:36:24 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
-
 /**
- * * main - Printing
- * *
- * * Return: 0 (Successful)
- * */
+ *  * main - main block
+ *   * Return: 0
+ *    */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual
-			puzzle");
-	return (0);
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
 }
