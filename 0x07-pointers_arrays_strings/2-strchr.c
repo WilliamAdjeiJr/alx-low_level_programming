04f89d41757d0341a75a973bc0baebc1137ac201
William Adjei Jr <williamadjeijunior@gmail.com>
2022-09-26 09:56:39 -0700

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,8 +11,9 @@
  * @c: The character to be located.
  *
  * Return: If c is found - a pointer to the first occurence.
- * If c is not found - NULL.
+ *	If c is not found - NULL.
  */
+
 char *_strchr(char *s, char c)
 {
 	 int index;
