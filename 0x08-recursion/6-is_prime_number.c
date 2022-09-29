ac637654b25392591b2abbda14faa5813934baea
William Adjei Jr <williamadjeijunior@gmail.com>
2022-09-28 21:25:52 -0700

    Inception. Is it possible?

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,15 +8,15 @@
  */
 int is_prime(unsigned int n, unsigned int c)
 {
-	if(n%c ==0)
+	if (n % c == 0)
 	{
-		if(n==c)
+		if (n == c)
 			return (1);
 		else
 			return (0);
 	}
 
-	return(0 + is_prime(n, c + 1));
+	return (0 + is_prime(n, c + 1));
 }
 /**
  * is_prime_number-detects if an imput number is a prime number.
@@ -25,11 +25,11 @@ int is_prime(unsigned int n, unsigned int c)
  */
 int is_prime_number(int n)
 {
-	if(n==0)
+	if (n == 0)
 		return (0);
-	if(n<0)
+	if (n < 0)
 		return (0);
-	if(n==1)
+	if (n == 1)
 		return (0);
 	return (is_prime(n, 2));
 }
